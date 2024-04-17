3ec74ca1e98577e04f40ad6baee26929438ad4ce
Hny274 <hunnygandhi274@gmail.com>
2024-04-17 13:59:02 +0530

    database connected

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { connectToMongo } = require("./Database/connectDb");
 
 dotenv.config();
+connectToMongo();
 
 const port = process.env.PORT || 5000;
 const app = express();
